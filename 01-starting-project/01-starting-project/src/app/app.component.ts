2b9531736df2e61e2b64a6b1151e4da04d64d43f
Gustavo Xavier <gus.guilhermexavier08@gmail.com>
2024-07-03 18:02:29 -0300

    tasks

--- 01-starting-project/01-starting-project/src/app/app.component.ts
+++ 01-starting-project/01-starting-project/src/app/app.component.ts
@@ -17,7 +17,7 @@ users = DUMMY_USERS;
 selectedUserId = 'u1';
 
 get selectedUser(){
-  return this.users.find((user) => user.id === this.selectedUserId)!;
+  return this.users.find((user) => user.id === this.selectedUserId);
 }
 
 onSelectedUser(id:string){
