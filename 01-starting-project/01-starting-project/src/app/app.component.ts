4b8eee02045e599235eae642d3a39d1bd7bd736b
Gustavo Xavier <gus.guilhermexavier08@gmail.com>
2024-07-03 17:12:25 -0300

    novo componente

--- 01-starting-project/01-starting-project/src/app/app.component.ts
+++ 01-starting-project/01-starting-project/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import {DUMMY_USERS} from './dummy-users'
+import { TasksComponent } from "./tasks/tasks.component";
 
 
 @Component({
@@ -9,13 +10,18 @@ import {DUMMY_USERS} from './dummy-users'
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [HeaderComponent, UserComponent]
+    imports: [HeaderComponent, UserComponent, TasksComponent]
 })
 export class AppComponent {
 users = DUMMY_USERS;
+selectedUserId = 'u1';
+
+get selectedUser(){
+  return this.users.find((user) => user.id === this.selectedUserId)!;
+}
 
 onSelectedUser(id:string){
-console.log('Selected user with id '+ id);
+this.selectedUserId = id;
 }
 
 }
