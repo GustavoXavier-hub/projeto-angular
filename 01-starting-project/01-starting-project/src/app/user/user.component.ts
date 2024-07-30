4fa902ac0f05f667a4908ff7f4c8663919c06ed9
Gustavo Xavier <gus.guilhermexavier08@gmail.com>
2024-07-30 17:58:44 -0300

    novo componente

--- 01-starting-project/01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
-import { Component,EventEmitter,Input,Output,input,output} from '@angular/core';
+import { Component,EventEmitter,Input,Output} from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 
 
@@ -7,7 +8,8 @@ import { type User } from './user.model';
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
+  imports: [CardComponent]
 })
 export class UserComponent {
 @Input({required : true}) user!: User;
